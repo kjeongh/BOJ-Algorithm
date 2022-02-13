109178402faa9f5a62d3e033572af0a81a533c97
kjeongh <wellsy001012@gmail.com>
2022-02-13 14:48:00 +0900

    1009번 문제 최종 수정

--- 1009.py
+++ 1009.py
@@ -1,4 +1,9 @@
-#잘 되나 확인용~
-print("gg")
-print("엥 왜 안돼")
-print("뒤진다")
+import sys
+case = int(input())
+arr = []
+for x in range(0, case):
+    arr.append(input().split())
+for y in range(len(arr)):
+    com_num = int(arr[y][0])**int(arr[y][1])%10
+    if com_num == 0: print("10")
+    else: print(com_num)
